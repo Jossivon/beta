ac1c24235ae898f0201a7ca971a176720da9cabb
Jossivon <josselyn.barahona@espoch.edu.ec>
2019-06-12 19:58:26 -0500

    nuevo 2

--- pos/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once "controlador/controlador.plantilla.php";;
-	//require_once "vistas/datos.php"
-	//require_once "vistas/conexion.php";
-
-	$plantilla = new ControladorPlantilla();
-	$plantilla -> ctrPlantilla();
-
-	

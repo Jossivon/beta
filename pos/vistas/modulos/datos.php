f97d7191e6e093f8534bcd3ec4bf374deef26a09
Jossivon <josselyn.barahona@espoch.edu.ec>
2019-06-12 19:17:15 -0500

    nuevo

---
+++ pos/vistas/modulos/datos.php
@@ -0,0 +1,11 @@
+<?php
+
+    /*$hostname="webapp.espoch.edu.ec";
+    $database="baseapp20";
+    $username="user20";
+    $password="ivonne08";*/
+
+    $hostname="localhost";
+    $database="proyecto";
+    $username="root";
+    $password="";
